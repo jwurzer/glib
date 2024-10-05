ef146db2e14896a54735984ae237597819da25fd
jwurzer <jwurzer.development@gmail.com>
2024-10-05 07:13:48 +0200

    Apply patch for gio/xdgmime/xdgmime.c

--- gio/xdgmime/xdgmime.c
+++ gio/xdgmime/xdgmime.c
@@ -228,7 +228,7 @@ xdg_init_dirs (void)
   xdg_data_dirs = getenv ("XDG_DATA_DIRS");
 
   if (xdg_data_dirs == NULL)
-    xdg_data_dirs = "/usr/local/share/:/usr/share/";
+    xdg_data_dirs = "/Applications/PCManFM.app/Contents/Resources/share/:/usr/share/";
 
   /* Work out how many dirs we’re dealing with. */
   if (xdg_data_home != NULL || home != NULL)
